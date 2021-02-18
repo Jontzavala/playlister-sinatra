44e343468c3a320ab50588cc69f47b25e1ed60ab
Jonathan Zavala <jontzavala@gmail.com>
2021-02-18 12:19:53 -0600

    commit tree fa53c55678f2813bca8043f71c739dabee9a280b

---
+++ app/models/song.rb
@@ -0,0 +1,8 @@
+class Song < ActiveRecord::Base
+    belongs_to :artist
+    has_many :song_genres
+    has_many :genres, through: :song_genres
+    extend Sluggable::ClassMethods #self.find_by_slug
+    include Sluggable::InstanceMethods #slug
+
+end
